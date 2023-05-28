135c7345c5c21d8b5246cee1308b3322b473f12f
nikhil123nikhil <106946553+nikhil123nikhil@users.noreply.github.com>
2023-05-28 06:11:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 sudo_users = [829259422,1984763765,2009124246,2027883161,5338416066]
 
-iptv_link = "https://gist.githubusercontent.com/nikhil123nikhil/74fd13528fdc6d47d4855b50ea672ef1/raw/56d8d4fa8e8e0dfe528340c83ac2169bdfce2047/iptv.json"
+iptv_link = "https://gist.githubusercontent.com/nikhil123nikhil/74fd13528fdc6d47d4855b50ea672ef1/raw/5d43784e5e558ab88c4377c328eaff5b7fee75df/iptv.json"
 
 GROUP_TAG = "Nikhil"
 
